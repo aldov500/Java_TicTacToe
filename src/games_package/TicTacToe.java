9893afb6ef4a8d35c20997fd1a7686c06ef0d0d2
Aldo Vargas <aldo.vargas.meza94@gmail.com>
2021-04-09 14:36:43 -0500

    Board with matriz and win procedure

--- src/games_package/TicTacToe.java
+++ src/games_package/TicTacToe.java
@@ -4,18 +4,20 @@ import java.util.Hashtable;
 public class TicTacToe {
 	
 	public boolean corriendo;
-	private char[] tablero;
+	private char[][] tablero;
+	int ganador;
 	
 	Hashtable<Integer,Character> jugadores;
 	
 	public TicTacToe() {
 		
-		tablero = new char[9];
+		tablero   = new char[3][3];
 		jugadores = new Hashtable<Integer,Character>();
 		
 		jugadores.put(1,'X');
 		jugadores.put(2,'O');
 		
+		ganador = 0;
 		corriendo = true;
 	}
 	
@@ -43,21 +45,120 @@ public class TicTacToe {
 		System.out.println("Player 2 O");
 		
 		
-		System.out.println("||___ ___ ___||");
-		System.out.println("||   |   |   ||");
-		System.out.println("||_" + tablero[0] +"_|_" + tablero[1] + "_|_" + tablero[2] + "_||");
-		System.out.println("||   |   |   ||");
-		System.out.println("||_" + tablero[3] +"_|_" + tablero[4] + "_|_" + tablero[5] + "_||");
-		System.out.println("||   |   |   ||");
-		System.out.println("||_" + tablero[6] +"_|_" + tablero[7] + "_|_" + tablero[8] + "_||");
-		System.out.println("||           ||");
+		System.out.println("||___ ___ ___||");		
+		System.out.printf("||_%c_|_%c_|_%c_||\n",tablero[0][0],tablero[0][1],tablero[0][2]);
+		System.out.printf("||_%c_|_%c_|_%c_||\n",tablero[1][0],tablero[1][1],tablero[1][2]);
+		System.out.printf("||_%c_|_%c_|_%c_||\n",tablero[2][0],tablero[2][1],tablero[2][2]);
 	}
 
-	public void MovimientoNuevo(int jugador, int posicion) {
-		tablero[posicion] = jugadores.get(jugador);
+	public boolean MovimientoNuevo(int jugador, int posicion) {
+		int x = 0, y = 0;
+		switch(posicion) {
+		case 1:
+			x = 0;
+			y = 0;
+			break;
+		case 2:
+			x = 0;
+			y = 1;
+			break;
+		case 3:
+			x = 0;
+			y = 2;
+			break;
+		case 4:
+			x = 1;
+			y = 0;
+			break;
+		case 5:
+			x = 1;
+			y = 1;
+			break;
+		case 6:
+			x = 1;
+			y = 2;
+			break;
+		case 7:
+			x = 2;
+			y = 0;
+			break;
+		case 8:
+			x = 2;
+			y = 1;
+			break;
+		case 9:
+			x = 2;
+			y = 2;
+			break;		
+		}
+		tablero[x][y] = jugadores.get(jugador);
+		
+		int posible_ganador = RevisarGanador(jugador);
+		if(posible_ganador != 0) {
+			System.out.printf("Ganador\n");
+			ganador = posible_ganador;
+			return true;
+		}else {
+			return false;
+		}
+			
 	}
 	
 	public void TerminarJuego() {
 		corriendo = false;
+		if(ganador!= 0) {
+			System.out.println("El ganador fue " + ganador);
+		}
 	}
+	
+	private int RevisarGanador(int jugador) {
+		int ganador = 0;
+		int token_consecutivo = 0;
+		char token_jugador = jugadores.get(jugador);
+		
+		// Revisar las Columnas
+		for(int fila=0; fila<3; fila++){
+			for(int col=0; col<3; col++) {
+				if(tablero[fila][col] == token_jugador) {
+					token_consecutivo += 1;
+				}
+			}
+			if(token_consecutivo > 2) {
+				ganador = jugador;
+			}else {
+				token_consecutivo = 0;
+			}
+		}
+		
+		// Revisar las Filas
+		for(int col=0; col<3; col++){
+			for(int fila=0; fila<3; fila++) {
+				if(tablero[fila][col] == token_jugador) {
+					token_consecutivo += 1;
+				}
+			}
+			if(token_consecutivo > 2) {
+				ganador = jugador;
+			}else {
+				token_consecutivo = 0;
+			}
+		}
+		// Revisar Diagonales
+		
+		for(int fila=0; fila<3; fila++) {
+			if(tablero[fila][fila] == token_jugador) {
+				token_consecutivo += 1;
+			}
+		}
+		if(token_consecutivo > 2) {
+			ganador = jugador;
+		}else {
+			token_consecutivo = 0;
+		}
+		
+		// Revisar las Diagonales Invertidas
+		
+		return ganador;
+	}
+	
 }
--- src/main.java
+++ src/main.java
@@ -10,34 +10,37 @@ public class main {
 		
 		Scanner entrada_jugador = new Scanner(System.in);
 		Scanner posicion = new Scanner(System.in);
-		
-		String opcion = "A";
-		System.out.println("Presione P ...");
-		opcion = entrada_jugador.nextLine();
+
 		
 		while(juego.corriendo == true) {
 			
+			int p = 0;
 			
-			if(opcion.equals("P")) {
-				
-				int p = 0;
-						
+			juego.RefrescarTablero();
+			System.out.println("El turno es del jugador 1 :");
+			p = posicion.nextInt();
+			if (juego.MovimientoNuevo(1, p) == true) {
 				juego.RefrescarTablero();
-				System.out.println("El turno es del jugador 1 :");
-				p = posicion.nextInt();
-				juego.MovimientoNuevo(1, p);
-				
-				juego.RefrescarTablero();
-				System.out.println("El turno es del jugador 2 :");
-				p = posicion.nextInt();
-				juego.MovimientoNuevo(2, p);
-				
+			
+				juego.TerminarJuego();
+				break;
 			}
 			
+			juego.RefrescarTablero();
+			System.out.println("El turno es del jugador 2 :");
+			p = posicion.nextInt();
+			if (juego.MovimientoNuevo(2, p) == true) {
+				juego.RefrescarTablero();
+			
+				juego.TerminarJuego();
+				break;
+			}
+		
 		}
 		
 		posicion.close();
 		entrada_jugador.close();
+		
 	}
 	
 }
